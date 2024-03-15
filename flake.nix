a53c6ce141024f1e2f7f3cfb4dcc9f1a9d144637
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-03-15 06:41:00 +0100

    Setup. System works.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+#    home-manager.url = "github:nix-community/home-manager/release-23.11";
+#    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+#  outputs = { nixpkgs, home-manager, ... }: 
+  outputs = { nixpkgs, ... }: 
+
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+
+    nixosConfigurations = {
+# TODO please change the hostname to your own
+      nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+
+  };
+}
