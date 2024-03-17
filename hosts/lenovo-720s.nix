066c76e6cca9c93ed2902ab2ae43ddce8e2e55ce
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-03-17 21:47:54 +0100

    Syncthing default settings

--- hosts/lenovo-720s.nix
+++ hosts/lenovo-720s.nix
@@ -22,6 +22,10 @@
   ];
 
   # Syncthing
-  services.syncthing.enable = true;
-
+  services.syncthing = {
+    enable = true;
+    user = "conny";
+    dataDir = "/home/conny";    # Default folder for new synced folders
+    configDir = "/home/conny/.config/syncthing";   # Folder for Syncthing's settings and keys
+  };
 }
