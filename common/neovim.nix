1b6d787ba05a207dbf1871da21f4be3a876dec95
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-04-07 11:39:42 +0200

    Added md linting

--- common/neovim.nix
+++ common/neovim.nix
@@ -4,7 +4,8 @@
   environment.systemPackages = with pkgs; [
     neovim
     python311Packages.python-lsp-server
-    lua-language-server
-    pylint
+    lua-language-server   # Python LSP server
+    pylint                # Linting for Python
+    markdownlint-cli2     # Linting for markdown
   ];
 }
