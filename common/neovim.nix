590f913fe8750930fbf635c30f8ec449823d8ab0
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-04-10 16:20:43 +0200

    Lazygit added
    Also trying out lazygit with writing this commit description.

--- common/neovim.nix
+++ common/neovim.nix
@@ -2,10 +2,15 @@
 
 {
   environment.systemPackages = with pkgs; [
+    
     neovim
-    python311Packages.python-lsp-server
-    lua-language-server   # Python LSP server
+
+    python311Packages.python-lsp-server   # Python LSP server
+    lua-language-server                   # Lua LSP server
+  
     pylint                # Linting for Python
     markdownlint-cli2     # Linting for markdown
+
+    lazygit               # Terminal UI for git commands
   ];
 }
