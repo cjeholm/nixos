26ede69b7543bfd8c777fd3914bf4a26c3679569
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-06-01 19:54:12 +0200

    Added hints for udev rules config

--- common/displaycal.nix
+++ common/displaycal.nix
@@ -8,6 +8,9 @@
 
   ];
 
+  # udev rule for usb instrument permission
+  # get idVendor and idProduct with 'sudo dmesg' 
+
   services.udev.extraRules = ''
     SUBSYSTEM=="usb", ACTION=="add", ATTR{idVendor}=="0765", ATTR{idProduct}=="5020", TAG+="uaccess"
   '';
