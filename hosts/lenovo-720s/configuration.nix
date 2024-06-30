89be978024980293f838cb1e43cd0c8758e3b1f0
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-06-30 16:39:03 +0200

    Updated syntax

--- hosts/lenovo-720s/configuration.nix
+++ hosts/lenovo-720s/configuration.nix
@@ -54,7 +54,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
   # Enable qtile
   services.xserver.windowManager.qtile.enable = true;
