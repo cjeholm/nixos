535324b870cebef5f18816fbfe65627aec44221a
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-07-19 13:41:00 +0200

    renamed option

--- common/i915.nix
+++ common/i915.nix
@@ -16,7 +16,7 @@
         VDPAU_DRIVER = lib.mkIf config.hardware.opengl.enable (lib.mkDefault "va_gl");
       };
 
-      hardware.opengl.extraPackages = with pkgs; [
+      hardware.graphics.extraPackages = with pkgs; [
         (if (lib.versionOlder (lib.versions.majorMinor lib.version) "23.11") then vaapiIntel else intel-vaapi-driver)
         libvdpau-va-gl
         intel-media-driver
