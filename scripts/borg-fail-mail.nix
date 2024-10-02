b6c48b16da8ea1e2d23d7b149f61528b41e345d6
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-10-02 09:52:36 +0200

    Added a notify-send

--- scripts/borg-fail-mail.nix
+++ scripts/borg-fail-mail.nix
@@ -2,4 +2,5 @@
 
 pkgs.writeShellScriptBin "borg-fail-mail" ''
   /run/current-system/sw/bin/echo \"The BorgBackup service failed. Check the logs for details.\" | /run/current-system/sw/bin/mail -s \"BorgBackup_failed\" conny@localhost
+  /run/current-system/sw/bin/notify-send "Borg Backup" "Service failed. Check the logs" -u critical
 ''
