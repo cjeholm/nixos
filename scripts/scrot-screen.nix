222520a26646cef75cf19686f68df93d3cb7cd26
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-10-04 13:20:21 +0200

    Notifications for screenshots saved

--- scripts/scrot-screen.nix
+++ scripts/scrot-screen.nix
@@ -2,4 +2,5 @@
 
 pkgs.writeShellScriptBin "scrot-screen" ''
   scrot ~/Pictures/screenshots/%Y-%m-%d_%H%M%S.png
+  notify-send "Fullscreen screenshot" "Saved in Pictures/screenshots"
 ''
--- scripts/scrot-window.nix
+++ scripts/scrot-window.nix
@@ -2,4 +2,5 @@
 
 pkgs.writeShellScriptBin "scrot-window" ''
   scrot --focused ~/Pictures/screenshots/%Y-%m-%d_%H%M%S.png
+  notify-send "Window screenshot" "Saved in Pictures/screenshots"
 ''
