0f151f28842a52c4aa28d693254aeb8904611dc2
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-11-27 23:40:15 +0100

    Added wacom stuff for screen annotations with gromit-mpx

--- common/wacom.nix
+++ common/wacom.nix
@@ -5,15 +5,4 @@
 
   # Enable Wacom tablet
   services.xserver.wacom.enable = true;
-
-  # Enable the script to run at startup.
-
-  systemd.services.wacom-touch-off = {
-    description = "Disable Wacom Touch";
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      ExecStart = "${pkgs.wacomtablet}/bin/xsetwacom --set 27 Touch off";
-      Type = "simple"; # Using "Simple" so systemd won't wait for script to finish before proceeding
-    };
-  };
 }
