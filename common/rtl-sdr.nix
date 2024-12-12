48edf3c43591225065e4efb3810c5b9154c51f6b
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-12-12 20:56:07 +0100

    DVB kernel module blacklisted. Added udev via settings

--- common/rtl-sdr.nix
+++ common/rtl-sdr.nix
@@ -8,9 +8,13 @@
     pkgs-stable.sdrangel
   ];
 
+  # Blacklist the DVB driver
+  boot.blacklistedKernelModules = ["dvb_usb_rtl28xxu"];
+
+  # services.udev.packages = [pkgs.rtl-sdr];
+
   # udev rule for usb instrument permission
   # get idVendor and idProduct with 'sudo dmesg'
-
   # RTL2832U OEM vid/pid, e.g. ezcap EzTV668 (E4000), Newsky TV28T (E4000/R820T) etc.
   services.udev.extraRules = ''
     SUBSYSTEMS=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="2838", MODE:="0666"
