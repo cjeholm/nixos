ce86cd48a477900d478cb4f4397eb8349093019c
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2024-12-19 11:06:23 +0100

    Flake updated. Stable set to 24.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Conny Holms NixOs Config";
 
   inputs = {
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-pinned.url = "github:nixos/nixpkgs/9cba8883bbb694f4cc3c517abfb5c0b11558943b";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    # nixpkgs-pinned.url = "github:nixos/nixpkgs/9cba8883bbb694f4cc3c517abfb5c0b11558943b";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
@@ -12,7 +12,7 @@
   outputs = inputs @ {
     nixpkgs,
     nixpkgs-stable,
-    nixpkgs-pinned,
+    # nixpkgs-pinned,
     ...
   }: let
     system = "x86_64-linux";
@@ -22,21 +22,10 @@
       config.allowUnfree = true;
     };
     pkgs-stable = import nixpkgs-stable commonArgs;
-    pkgs-pinned = import nixpkgs-pinned commonArgs;
+    # pkgs-pinned = import nixpkgs-pinned commonArgs;
   in {
     nixosConfigurations = {
       # Configurrations by hostname or '--flake .#name'
-
-      nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./common/common.nix
-        ];
-        specialArgs = {
-          inherit pkgs-stable;
-          inherit inputs;
-        };
-      };
 
       HolmDesktop = nixpkgs.lib.nixosSystem {
         inherit system;
@@ -45,7 +34,7 @@
         ];
         specialArgs = {
           inherit pkgs-stable;
-          inherit pkgs-pinned;
+          # inherit pkgs-pinned;
           inherit inputs;
         };
       };
