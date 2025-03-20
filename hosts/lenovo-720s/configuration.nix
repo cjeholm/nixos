9b47b927d0836e7c4186bc4bc357e8346738324d
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2025-03-20 21:58:42 +0100

    removed sddm

--- hosts/lenovo-720s/configuration.nix
+++ hosts/lenovo-720s/configuration.nix
@@ -52,7 +52,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = false;
   services.xserver.desktopManager.plasma5.enable = false;
   services.desktopManager.plasma6.enable = false;
 
