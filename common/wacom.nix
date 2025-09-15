c46f55438a257e0d310d1d45f07206e702058e04
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2025-09-15 16:45:15 +0200

    tablet stuff. doesnt work well.

--- common/wacom.nix
+++ common/wacom.nix
@@ -1,8 +1,12 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
+    # config.boot.kernelPackages.digimend
     # wacomtablet
   ];
 
   # Enable Wacom tablet
   services.xserver.wacom.enable = true;
+
+  hardware.opentabletdriver.enable = true;
+  services.xserver.digimend.enable = true;
 }
