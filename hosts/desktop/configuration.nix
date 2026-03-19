58af3c93154a6d2562032078483fd1936b2a1a5a
Conny Holm <59372304+cjeholm@users.noreply.github.com>
2026-03-19 17:40:00 +0100

    flake update

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -84,6 +84,8 @@
     ];
   };
 
+  services.openssh.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
